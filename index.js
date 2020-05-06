f11eb9a6340ce7447a876d97d6e8a154910fbedf
tippitytapp <marc.tapp@gmail.com>
2020-05-06 13:25:04 -0400

    got some of the middleware working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./server.js');
+const colors = require('colors');
+const path = require('path');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+const PORT = 4503;
+server.listen(PORT, () => {
+  console.log(`\n* Server Running on http://localhost:${PORT} *\n`.bgGreen.black.bold);
 });
--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express'); // importing a CommonJS module
-
+const colors = require('colors');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+// global middleware
+server.use(express.json()); // built in middleware, no need to mpn install it
 
-server.use('/api/hubs', hubsRouter);
+server.use(function(req, res, next){
+  const today = new Date().toISOString();
+  console.log(`A Request Just Happened on ${today} with a ${req.method} method to "${req.url}"`.bgWhite.black)
+  next();
+})
+
+server.use('/api/hubs', gate, role("fellowship"),hubsRouter);
+
+
+server.use(gate);
+
+/*
+check the headers to see if there is a password property.
+if there is, call next();
+otherwise return status 401 and you {you : "cannot pass!!"} 
+if there is no password return status 400 and {message: "speak friend and enter"}*/
+
+
+server.get('/moria', (req, res)=>{
+  res.status(200).json({
+    status: 200,
+    message: "OK",
+    response: " welcome friends!"
+  })
+})
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
@@ -16,5 +41,31 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+function role(roleName){
+  return function (req, res, next){
+    let role = req.headers.role;
+    if (role ===roleName){
+      next();
+    }else{
+      res.status(403).json({
+        you: "have no power"
+      })
+    }
+  }
+}
+function gate(req, res, next){
+  if(!req.headers.password){
+    res.status(401).json({
+      you: "cannot pass!!"
+    })
+  }else if(req.headers.password !== "mellons"){
+    res.status(400).json({
+      message: "speak friend and enter!"
+    })
+  }else{
+    next();
+  }
+}
 
 module.exports = server;
